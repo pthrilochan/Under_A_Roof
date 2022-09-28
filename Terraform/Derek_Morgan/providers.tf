2d2d3edfad701f4342f12eba46a7fa8ef2a0db69
pthrilochan <weblogic.xml@gmail.com>
2022-09-28 12:32:57 +0530

    Derek_YT1

---
+++ Terraform/Derek_Morgan/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  shared_config_files      = ["/home/thril/.aws/config"]
+  shared_credentials_files = ["/home/thril/.aws/credentials"]
+  profile                  = "tfadmin"
+}
